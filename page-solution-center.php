b87cc7a6e6ef262d286d757cf8c2ee8556318c6d
Fox Scarlett <foxontheone@gmail.com>
2019-12-08 19:24:55 -0800

    solution center inputs, wip

--- page-solution-center.php
+++ page-solution-center.php
@@ -50,23 +50,23 @@ get_header();
 								
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom1" value="hard_water" id="1"><label for="1">Hard Water</label>
+		              <label for="1" class="inputprob"><input type="checkbox" name="ic_symptom1" value="hard_water" id="1">Hard Water</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom2" value="dry_skin" id="2"><label for="2">Dry Skin / Hair</label>
+		              <label for="2" class="inputprob"><input type="checkbox" name="ic_symptom2" value="dry_skin" id="2">Dry Skin / Hair</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom3" value="stiff_laundry" id="3"><label for="3">Stiff Laundry</label>
+		              <label for="3" class="inputprob"><input type="checkbox" name="ic_symptom3" value="stiff_laundry" id="3">Stiff Laundry</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom4" value="soap_residue" id="4"><label for="4">Soap Scum or Residue</label>
+		              <label for="4" class="inputprob"><input type="checkbox" name="ic_symptom4" value="soap_residue" id="4">Soap Scum or Residue</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom5" value="glass_spots" id="5"><label for="5">Spotty, Cloudy Glassware</label>
+		              <label for="5" class="inputprob"><input type="checkbox" name="ic_symptom5" value="glass_spots" id="5">Spotty, Cloudy Glassware</label>
 		            </div>
 
 		            
@@ -76,19 +76,19 @@ get_header();
 								<div class="input-wrapper input-heading">By Smell / Taste</div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom11" value="smell_chlorine" id="11"><label for="11">Chlorine Smell</label>
+		              <label for="11" class="inputprob"><input type="checkbox" name="ic_symptom11" value="smell_chlorine" id="11">Chlorine Smell</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom25" value="smell_egg" id="25"><label for="25">Rotten Egg Smell</label>
+		              <label for="25" class="inputprob"><input type="checkbox" name="ic_symptom25" value="smell_egg" id="25">Rotten Egg Smell</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom13" value="taste_salty" id="13"><label for="13">Salty Taste</label>
+		              <label for="13" class="inputprob"><input type="checkbox" name="ic_symptom13" value="taste_salty" id="13">Salty Taste</label>
 		            </div>                        
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom14" value="taste_bitter" id="14"><label for="14">Bitter Taste</label>
+		              <label for="14" class="inputprob"><input type="checkbox" name="ic_symptom14" value="taste_bitter" id="14">Bitter Taste</label>
 		            </div>
 
 		          </div>
@@ -98,19 +98,19 @@ get_header();
 		            <div class="input-wrapper input-heading">By Appearance</div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom22" value="rusty" id="22"><label for="22">Rusty Stains</label>
+		              <label for="22" class="inputprob"><input type="checkbox" name="ic_symptom22" value="rusty" id="22">Rusty Stains</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom26" value="bluegreen" id="26"><label for="26">Blue / Green Stain</label>
+		              <label for="26" class="inputprob"><input type="checkbox" name="ic_symptom26" value="bluegreen" id="26">Blue / Green Stain</label>
 		            </div>
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom27" value="colored" id="27"><label for="27">Colored Water</label>
+		              <label for="27" class="inputprob"><input type="checkbox" name="ic_symptom27" value="colored" id="27">Colored Water</label>
 		            </div>                        
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom28" value="laundry_stains" id="28"><label for="28">Laundry Stains</label>
+		              <label for="28" class="inputprob"><input type="checkbox" name="ic_symptom28" value="laundry_stains" id="28">Laundry Stains</label>
 		            </div>
 
 
@@ -119,23 +119,23 @@ get_header();
 
 
 								<div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom15" value="lead" id="15"><label for="15">Lead</label>
+		              <label for="15" class="inputprob"><input type="checkbox" name="ic_symptom15" value="lead" id="15">Lead</label>
 		            </div>
 
 		            <div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom16" value="chromium" id="16"><label for="16">Chromium-6</label>
+		              <label for="16" class="inputprob"><input type="checkbox" name="ic_symptom16" value="chromium" id="16">Chromium-6</label>
 		            </div>
 
 		            <div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom23" value="arsenic" id="23"><label for="23">Arsenic</label>
+		              <label for="23" class="inputprob"><input type="checkbox" name="ic_symptom23" value="arsenic" id="23">Arsenic</label>
 		            </div>
 
 		            <div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom21" value="ph" id="21"><label for="21">pH ( Acidic / Alkaline )</label>
+		              <label for="21" class="inputprob"><input type="checkbox" name="ic_symptom21" value="ph" id="21">pH ( Acidic / Alkaline )</label>
 		            </div>
 
 		            <div class="input-wrapper">
-		              <input type="checkbox" name="ic_symptom6" value="tds" id="6"><label for="6">Total Dissolved Solids (TDS)</label>
+		              <label for="6" class="inputprob"><input type="checkbox" name="ic_symptom6" value="tds" id="6">Total Dissolved Solids (TDS)</label>
 		            </div>
 
 
@@ -146,7 +146,20 @@ get_header();
 						<button type="submit" class="solution-button">Get Solution!</button>
 
 				</form>
-					
+					<script>
+						// const formInput = document.querySelectorAll('.inputprob');
+
+						// formInput.onclick =	function() {
+						// 	formInput.classList.add('ic-selected');							
+						// }
+
+						jQuery(".inputprob").on("click", function(){
+						  
+						  jQuery(this).toggleClass("ic-selected", true);
+						});
+
+
+					</script>
 
 
 
