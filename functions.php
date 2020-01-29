9436610fea3c4e9bcb278ff5cadfd93ec512a3ea
Fox Scarlett <foxontheone@gmail.com>
2020-01-28 22:10:01 -0800

    WIP single product styles, enqueue bootstrap js, start FAQ accordion

--- functions.php
+++ functions.php
@@ -141,6 +141,20 @@ function isings_scripts() {
 
 	wp_enqueue_script( 'isings-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
+
+
+
+	// wp_enqueue_script( 'jquery-bs', 'https://code.jquery.com/jquery-3.4.1.slim.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'popper-bs', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(), '20151215', true );
+	wp_enqueue_script( 'js-bootstrap4', 'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(), '20151215', true );
+
+
+
+
+	// <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+// <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+// <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
